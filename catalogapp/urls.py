ab12bd7a08b19c74f07cf475212ef478a02fd20e
pekshevds <pekshev.ds@gmail.com>
2022-01-03 19:56:10 +0300

    20220103_003 finaly for now

--- catalogapp/urls.py
+++ catalogapp/urls.py
@@ -1,14 +1,21 @@
 from django.urls import path
 from .views import index
 from .views import APIBrand, APIManufacturer, APICountry, APIUnit, APICategory, APIProduct
+from .views import CountryGenerator
 
 urlpatterns = [
     path('', index, name='index-page'),
-    path('api/v1/brand/',           APIBrand.as_view()),
-    path('api/v1/brand/<str:id>/',  APIBrand.as_view()),
-    path('api/v1/manufacturer/',    APIManufacturer.as_view()),
-    path('api/v1/country/',         APICountry.as_view()),
-    path('api/v1/unit/',            APIUnit.as_view()),
-    path('api/v1/category/',        APICategory.as_view()),
-    path('api/v1/product/',         APIProduct.as_view()),
+    path('api/v1/brand/',                   APIBrand.as_view()),
+    path('api/v1/brand/<str:id>/',          APIBrand.as_view()),
+    path('api/v1/manufacturer/',            APIManufacturer.as_view()),
+    path('api/v1/manufacturer/<str:id>/',   APIManufacturer.as_view()),
+    path('api/v1/country/',                 APICountry.as_view()),
+    path('api/v1/countryes/',               CountryGenerator.as_view()),
+    path('api/v1/country/<str:id>/',        APICountry.as_view()),
+    path('api/v1/unit/',                    APIUnit.as_view()),
+    path('api/v1/unit/<str:id>/',           APIUnit.as_view()),
+    path('api/v1/category/',                APICategory.as_view()),
+    path('api/v1/category/<str:id>/',       APICategory.as_view()),
+    path('api/v1/product/',                 APIProduct.as_view()),
+    path('api/v1/product/<str:id>/',        APIProduct.as_view()),
 ]
--- catalogapp/views.py
+++ catalogapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import generics
 
 from .models import Brand, Manufacturer, Country, Unit, Category, Product
 from .serializers import BrandSerializer, ManufacturerSerializer, CountrySerializer, UnitSerializer, CategorySerializer, ProductSerializer
@@ -18,131 +19,251 @@ def index(request):
 
 class APIBrand(APIView):
 
-    def get(self, reques, id=''):
+    def get(self, request, id=''):
         if id == '':
-            brands = Brand.objects.all()
-            serialaizer = BrandSerializer(brands, many=True)
+            items = Brand.objects.all()
+            if len(items)> 0:
+                serializer = BrandSerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
         else:
             try:
-                brands = Brand.objects.get(id=id)
-            except:
+                items = Brand.objects.get(id=id)
+            except Brand.DoesNotExist:
                 return Response(status=status.HTTP_400_BAD_REQUEST) 
 
-            serialaizer = BrandSerializer(brands)
+            serializer = BrandSerializer(items)
         
-        return Response(serialaizer.data)
+        return Response(serializer.data)
 
     def post(self, request):
                 
         try:
-            brand = Brand.objects.get(id=request.data['id'])
+            item = Brand.objects.get(id=request.data['id'])
         except:
-            brand = None
+            item = None
 
-        if brand:#PUT
-            serialaizer = BrandSerializer(brand, data=request.data)
+        if item:#PUT
+            serializer = BrandSerializer(item, data=request.data)
         else:
-            serialaizer = BrandSerializer(data=request.data)
+            serializer = BrandSerializer(data=request.data)
                 
         
-        if serialaizer.is_valid():            
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class APIManufacturer(APIBrand):
+class APIManufacturer(APIView):
     
-    def get(self,request):
-        
-        manufactures = Manufacturer.objects.all()
-        serialaizer = ManufacturerSerializer(manufactures, many=True)
-        
-        return Response(serialaizer.data)
-    
-    def post(self, request):
-        
-        serialaizer = ManufacturerSerializer(data=request.data)
-        
-        if serialaizer.is_valid():
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
+    def get(self, request, id=''):
+        if id == '':
+            items = Manufacturer.objects.all()
+            if len(items)> 0:
+                serializer = ManufacturerSerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                items = Manufacturer.objects.get(id=id)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
 
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class APICountry(APIBrand):
-    
-    def get(self,request):
+            serializer = ManufacturerSerializer(items)
         
-        countryes = Country.objects.all()
-        serialaizer = CountrySerializer(countryes, many=True)
-        
-        return Response(serialaizer.data)
-    
-    def post(self, request):
-        
-        serialaizer = CountrySerializer(data=request.data)
-        
-        if serialaizer.is_valid():
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class APIUnit(APIBrand):
-    
-    def get(self,request):
-        
-        units = Unit.objects.all()
-        serialaizer = UnitSerializer(units, many=True)
-        
-        return Response(serialaizer.data)
+        return Response(serializer.data)
 
     def post(self, request):
-        
-        serialaizer = UnitSerializer(data=request.data)
-        
-        if serialaizer.is_valid():
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
+                
+        try:
+            item = Manufacturer.objects.get(id=request.data['id'])
+        except:
+            item = None
 
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if item:#PUT
+            serializer = ManufacturerSerializer(item, data=request.data)
+        else:
+            serializer = ManufacturerSerializer(data=request.data)
+                
+        
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class APICategory(APIBrand):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class APICountry(APIView):
     
-    def get(self,request):
+    def get(self, request, id=''):
+        if id == '':
+            items = Country.objects.all()
+            if len(items)> 0:
+                serializer = CountrySerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                items = Country.objects.get(id=id)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+            serializer = CountrySerializer(items)
         
-        categoryes = Category.objects.all()
-        serialaizer = CategorySerializer(categoryes, many=True)
-        
-        return Response(serialaizer.data)
-    
+        return Response(serializer.data)
+
     def post(self, request):
-        
-        serialaizer = CategorySerializer(data=request.data)
-        
-        if serialaizer.is_valid():
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
+                
+        try:
+            item = Country.objects.get(id=request.data['id'])
+        except:
+            item = None
 
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if item:#PUT
+            serializer = CountrySerializer(item, data=request.data)
+        else:
+            serializer = CountrySerializer(data=request.data)
+                
+        
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class APIProduct(APIBrand):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CountryGenerator(generics.ListCreateAPIView):
+    queryset = Country.objects.all()
+    serializer_class= CountrySerializer
+
+    def post(self, request, format='json'):
+        serializer= CountrySerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+
+    def put(self, request, format='json'):
+        serializer= CountrySerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_403_FORBIDDEN)
+
+
+class APIUnit(APIView):
     
-    def get(self,request):
+    def get(self, request, id=''):
+        if id == '':
+            items = Unit.objects.all()
+            if len(items)> 0:
+                serializer = UnitSerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                items = Unit.objects.get(id=id)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+            serializer = UnitSerializer(items)
         
-        products = Product.objects.all()
-        serialaizer = ProductSerializer(products, many=True)
-        
-        return Response(serialaizer.data)
-    
+        return Response(serializer.data)
+
     def post(self, request):
-        
-        serialaizer = ProductSerializer(data=request.data)
-        
-        if serialaizer.is_valid():
-            serialaizer.save()
-            return Response(serialaizer.data, status=status.HTTP_201_CREATED)
+                
+        try:
+            item = Unit.objects.get(id=request.data['id'])
+        except:
+            item = None
 
-        return Response(serialaizer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if item:#PUT
+            serializer = UnitSerializer(item, data=request.data)
+        else:
+            serializer = UnitSerializer(data=request.data)
+                
+        
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class APICategory(APIView):
+    
+    def get(self, request, id=''):
+        if id == '':
+            items = Category.objects.all()
+            if len(items)> 0:
+                serializer = CategorySerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                items = Category.objects.get(id=id)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+            serializer = CategorySerializer(items)
+        
+        return Response(serializer.data)
+
+    def post(self, request):
+                
+        try:
+            item = Category.objects.get(id=request.data['id'])
+        except:
+            item = None
+
+        if item:#PUT
+            serializer = CategorySerializer(item, data=request.data)
+        else:
+            serializer = CategorySerializer(data=request.data)
+                
+        
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class APIProduct(APIView):
+    
+    def get(self, request, id=''):
+        if id == '':
+            items = Product.objects.all()
+            if len(items)> 0:
+                serializer = ProductSerializer(items, many=True)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        else:
+            try:
+                items = Product.objects.get(id=id)
+            except:
+                return Response(status=status.HTTP_400_BAD_REQUEST) 
+
+            serializer = ProductSerializer(items)
+        
+        return Response(serializer.data)
+
+    def post(self, request):
+                
+        try:
+            item = Product.objects.get(id=request.data['id'])
+        except:
+            item = None
+
+        if item:#PUT
+            serializer = ProductSerializer(item, data=request.data)
+        else:
+            serializer = ProductSerializer(data=request.data)
+                
+        
+        if serializer.is_valid():            
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
