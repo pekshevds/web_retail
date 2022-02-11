41c8808154e741a0289c5f7d08157513d760707e
MaximPekshev <mp198769@gmail.com>
2022-02-11 16:12:37 +0300

    allauth

--- customerapp/urls.py
+++ customerapp/urls.py
@@ -1,21 +1,28 @@
 from django.urls import path
+from django.urls import include
 from .views import LoginView, LogoutView, ProfileView, PasswordChangeView, \
     PasswordChangeDoneView, PasswordResetView, PasswordResetCompleteView,\
          PasswordResetConfirmView, PasswordResetDoneView
-
+from .views import CustomLoginView, CustomSignupView
 
 urlpatterns = [
+
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('log/', CustomLoginView.as_view(), name='login'),
+    path('signup/', CustomSignupView.as_view(), name='signup'),
+
+    path(''		 		, include('allauth.urls')),
+
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
     
-    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    # path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    # path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
 
-    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
 
-    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     
 ]
--- customerapp/views.py
+++ customerapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth import views
 from django.views.generic.detail import DetailView
 
+from allauth.account.views import LoginView, SignupView
+from allauth.account.utils import passthrough_next_redirect_url, get_request_param
+from django.contrib.sites.shortcuts import get_current_site
 
 from .models import Customer
 # Create your views here.
@@ -10,7 +14,6 @@ from .models import Customer
 class ProfileView(DetailView):
     template_name = 'registration/profile.html'
     model = Customer
-
 
     def get_object(self, queryset=None):
         
@@ -57,8 +60,45 @@ class PasswordResetCompleteView(views.PasswordResetCompleteView):
     template_name = 'registration/password_reset_complete.html'
 
 
+class CustomLoginView(LoginView):
 
+	def get_context_data(self, **kwargs):
 
+		ret = super(LoginView, self).get_context_data(**kwargs)
+		signup_url = passthrough_next_redirect_url(self.request,reverse('account_signup'),self.redirect_field_name)
 
+		redirect_field_value = get_request_param(self.request,self.redirect_field_name)
+		site = get_current_site(self.request)
 
-    
+		ret.update({'signup_url': signup_url,
+                    'site': site,
+                    'redirect_field_name': self.redirect_field_name,
+                    'redirect_field_value': redirect_field_value,
+        			})
+
+		return ret
+
+class CustomSignupView(SignupView):
+
+	def get_context_data(self, **kwargs):
+		ret = super(SignupView, self).get_context_data(**kwargs)
+
+		form = ret['form']
+		email = self.request.session.get('account_verified_email')
+
+		if email:
+			email_keys = ['email']
+			if app_settings.SIGNUP_EMAIL_ENTER_TWICE:
+				email_keys.append('email2')
+			for email_key in email_keys:
+				form.fields[email_key].initial = email
+
+		login_url = passthrough_next_redirect_url(self.request,reverse("account_login"),self.redirect_field_name)
+		redirect_field_name = self.redirect_field_name
+		redirect_field_value = get_request_param(self.request,redirect_field_name)
+
+		ret.update({'login_url': login_url,
+	                'redirect_field_name': redirect_field_name,
+	                'redirect_field_value': redirect_field_value,
+	                })
+		return ret
